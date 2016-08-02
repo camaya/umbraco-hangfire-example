a5e5623554c6221d8b5c4444539a07c25fc421c8
Cristhian Amaya <cristhian@lecoati.com>
2016-08-02 17:21:45 +0200

    Add integration example between Hangfire and Umbraco

---
+++ UmbracoHangfireExample/App_Start/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Owin;
+using Umbraco.Web;
+using Hangfire;
+using UmbracoHangfireExample.Filters;
+
+namespace UmbracoHangfireExample.App_Start
+{
+    public class Startup : UmbracoDefaultOwinStartup
+    {
+
+        public override void Configuration(IAppBuilder app)
+        {
+            base.Configuration(app);
+
+            // Configure the database where Hangfire is going to create its tables
+            var cs = Umbraco.Core.ApplicationContext.Current.DatabaseContext.ConnectionString;
+            GlobalConfiguration.Configuration
+                .UseSqlServerStorage(cs);
+
+            var dashboardOptions = new DashboardOptions
+            {
+                Authorization = new[]
+                {
+                    new UmbracoUserAuthorizationFilter()
+                }
+            };
+            
+            // Configure Hangfire's dashboard with auth configuration
+            app.UseHangfireDashboard("/hangfire", dashboardOptions);
+
+            // Create a default Hangfire server
+			app.UseHangfireServer();
+        }
+
+    }
+}
---
+++ UmbracoHangfireExample/Filters/UmbracoUserAuthorizationFilter.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire.Dashboard;
+using System;
+using System.Linq;
+using System.Web;
+using Umbraco.Core;
+using Umbraco.Core.Security;
+using Hangfire.Annotations;
+
+namespace UmbracoHangfireExample.Filters
+{
+    public class UmbracoUserAuthorizationFilter : IDashboardAuthorizationFilter
+    {
+
+        public bool Authorize([NotNull] DashboardContext context)
+        {
+            var auth = new HttpContextWrapper(HttpContext.Current).GetUmbracoAuthTicket();
+            if (auth == null)
+            {
+                return false;
+            }
+
+            var backofficeUser = ApplicationContext.Current.Services.UserService.GetByUsername(auth.Name);
+            var isAllowed = backofficeUser != null &&
+                backofficeUser.AllowedSections.Any(x => x.Equals("developer", StringComparison.InvariantCultureIgnoreCase));
+            return isAllowed;
+        }
+
+    }
+}
